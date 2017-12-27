e9f2cea2edccbf17f16c1c98ca04791cde5d5b66
Hope <ybobby.biz@gmail.com>
2017-12-27 20:54:30 +0700

    first commit

---
+++ application/views/auth/login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>KANTOR IMIGRASI KELAS I BOGOR</title>
+
+    <meta http-equiv="X-UA-Compatible" content=="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/fonts/line-awesome/css/line-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/fonts/open-sans/styles.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/libs/tether/css/tether.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/styles/common.min.css">
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/styles/pages/auth.min.css">
+</head>
+<body>
+
+<div class="ks-page">
+    <div class="ks-page-header">
+        <a href="#" class="ks-logo">KANIM KELAS I BOGOR</a>
+    </div>
+    <div class="ks-page-content">
+        <div class="ks-logo">KANIM KELAS I BOGOR</div>
+
+        <div class="card panel panel-default ks-light ks-panel ks-login">
+            <div class="card-block">
+                <form method="post" action="login" class="form-container">
+                    <h4 class="ks-header">Login</h4>
+                    <div class="form-group">
+                        <div class="input-icon icon-left icon-lg icon-color-primary">
+                            <input type="text" name="identity" class="form-control" placeholder="Email">
+                            <span class="icon-addon">
+                                <span class="la la-at"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="input-icon icon-left icon-lg icon-color-primary">
+                            <input type="text" class="form-control" name="password" placeholder="Password">
+                            <span class="icon-addon">
+                                <span class="la la-key"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </div>
+                    <div class="ks-text-center">
+                        <a href="pages-forgot-password.html">Lupa Password</a>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="ks-footer">
+        <span class="ks-copyright">&copy; 2017 Hope ID</span>
+        <ul>
+            <li>
+                <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<script src="<?=base_url()?>assets/libs/jquery/jquery.min.js"></script>
+<script src="<?=base_url()?>assets/libs/tether/js/tether.min.js"></script>
+<script src="<?=base_url()?>assets/libs/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ application/views/main_lib.php
@@ -0,0 +1,22 @@
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="<?=base_url()?>assets/libs/jquery/jquery.min.js"></script>
+<script src="<?=base_url()?>assets/libs/responsejs/response.min.js"></script>
+<script src="<?=base_url()?>assets/libs/loading-overlay/loadingoverlay.min.js"></script>
+<script src="<?=base_url()?>assets/libs/tether/js/tether.min.js"></script>
+<script src="<?=base_url()?>assets/libs/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?=base_url()?>assets/libs/jscrollpane/jquery.jscrollpane.min.js"></script>
+<script src="<?=base_url()?>assets/libs/jscrollpane/jquery.mousewheel.js"></script>
+<script src="<?=base_url()?>assets/libs/flexibility/flexibility.js"></script>
+<script src="<?=base_url()?>assets/libs/noty/noty.min.js"></script>
+<script src="<?=base_url()?>assets/libs/velocity/velocity.min.js"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="<?=base_url()?>assets/scripts/common.min.js"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
+
+<script src="<?=base_url()?>assets/libs/d3/d3.min.js"></script>
+<script src="<?=base_url()?>assets/libs/c3js/c3.min.js"></script>
+<script src="<?=base_url()?>assets/libs/noty/noty.min.js"></script>
+<script src="<?=base_url()?>assets/libs/maplace/maplace.min.js"></script>
+<script src="https://maps.google.com/maps/api/js?libraries=geometry&v=3.26&key=AIzaSyBBjLDxcCjc4s9ngpR11uwBWXRhyp3KPYM"></script>
